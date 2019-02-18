abeac6b1432c150893416b092b7debe206a00f31
Natália Reiko Sakamoto <nataliareikosakamoto@gmail.com>
2019-02-18 12:07:54 -0300

    python exercicios

---
+++ index.py
@@ -0,0 +1,30 @@
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+
+# soma = n1 + n2 
+#     print(soma)
+# subtração = n1 - n2
+#     print(subtração)
+# multiplicação = n1 * n2 
+#     print(multiplicação)
+# divisão = n1 / n2
+#     print(divisão)
+
+# n3 = int(input("Digite o terceiro número: "))
+# n4 = int(input("Digite o quarto número: "))
+# media = ((n1 + n2 + n3 + n4) / 4)
+#     print(media)
+
+
+# C = float(input("Digite alguma temperatura em celcius: "))
+#     temperatura = (C* 9/5) + 32
+#     print(temperatura)
+
+
+# frase = (input("Digite alguma frase: "))
+# letra = (input("Digite alguma letra: "))
+#    print(frase.count(letra))
+
+
+qemedia = (input("Digite quantos elementos a média terá: "))
+listamedia = range(len(qemedia)
